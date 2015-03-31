9d7bba1a2b89d59bf4333f9b73a448d84c47abc7
hyperion70 <bmaximov71@gmail.com>
2015-03-31 09:17:09 +0300

    Fix building errors.

--- j608_fly-vendor.mk
+++ j608_fly-vendor.mk
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/mediatek/j608_fly/vendor-blobs.mk)
+$(call inherit-product, vendor/mediatek/j608_fly/j608_fly-blobs.mk)
